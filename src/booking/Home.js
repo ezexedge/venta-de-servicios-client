281373e8c4e98591cc1a60d6de5814ac0b78c706
juan gallardo <juan@texdinamo.com>
2021-01-29 22:12:25 -0300

    subiendo todo

---
+++ src/booking/Home.js
@@ -0,0 +1,13 @@
+import {useSelector , useStore} from 'react-redux'
+
+const Home = () => {
+
+ const {user} = useSelector(  ( state ) => ({...state}))
+    return ( 
+        <div className="container-fluid h1 p-5 text-center">
+            Home page {JSON.stringify(user)}
+        </div>
+     );
+}
+ 
+export default Home;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux'
+import {Provider } from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from './reducers'
+
+
+
+
+const store = createStore(rootReducer,composeWithDevTools())
+
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
