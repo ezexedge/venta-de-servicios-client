af3b2cede3da884695fdb7322fbbafe2912b3efd
juan gallardo <juan@texdinamo.com>
2021-01-30 17:11:58 -0300

    formulario

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,8 +1,48 @@
+import react , {useState} from 'react'
+
+
 const Register = () => {
+
+const [name,setName] = useState("")
+const [email,setEmail] = useState("")
+const [password,setPassword] = useState("")
+
+
+const handleSubmit = () => {
+    alert('enviando info al bakcednd')
+}
+
+const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+        <p>hola</p>
+        <button type="submit">submit</button>
+    </form>
+)
+
+
     return ( 
-        <div className="container-fluid h1 p-5 text-center">
-            Register
+        <>
+        <div className="container-fluid h1 bg-secondary p-5 text-center">
+            <h1>Registers</h1>
         </div>
+
+        <div className="container">
+
+                <div className="row">
+
+                        <div className="col-md-6 offset-md-3">
+
+                            {registerForm()}
+                <p>fojefojofjo</p>
+
+                        </div>
+
+
+                </div>
+
+        </div>
+
+        </>
      );
 }
  
